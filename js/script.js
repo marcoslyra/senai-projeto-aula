abc68b4800287427c790e052cc44574ebcbdd947
marcoslyra <markinhoskelly@outlook.com>
2021-11-03 21:47:00 -0300

    Aula0311

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*Script do Carrossel*/
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 2000,
+    keyboard: true
+    pause: 'hover'
+    wrap: false
+})
